854990bd8f870ec9f1d0e9352b56ad9af9c2bdd4
Umit Turk <uyt.turk@gmail.com>
2021-07-06 15:35:15 +0300

    3. homework

---
+++ odev3.sql
@@ -0,0 +1,6 @@
+--SELECT country FROM country
+--WHERE country LIKE 'A%a';
+--WHERE country LIKE '_____n';
+--SELECT * FROM film
+--WHERE title ILIKE '%t%t%t%t%';
+--WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
