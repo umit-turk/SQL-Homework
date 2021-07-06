1fbe2b824bf7808621078cf4fa341e87320bf8de
Umit Turk <uyt.turk@gmail.com>
2021-07-06 16:11:41 +0300

    4th homework

---
+++ odev4.sql
@@ -0,0 +1,7 @@
+--SELECT COUNT(DISTINCT replacement_cost) FROM film
+--SELECT COUNT(title) FROM film
+--WHERE title LIKE 'T%' AND rating = 'G';
+--SELECT COUNT(country) FROM country
+--WHERE country ILIKE '_____';
+--SELECT COUNT(city) FROM city
+--WHERE city ILIKE '%R';
